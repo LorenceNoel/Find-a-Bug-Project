4956f06711d6a28534f4e59a20a83f8cf69b156d
LorenceNoel <167948703+LorenceNoel@users.noreply.github.com>
2025-03-25 23:18:47 -0600

    GuessingGame Files

---
+++ app/src/main/java/com/example/assignment2_group2/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.assignment2_group2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnStartGame = findViewById(R.id.btnStartGame);
+        Button btnSettings = findViewById(R.id.btnSettings);
+        Button btnAbout = findViewById(R.id.btnAbout);
+
+        btnStartGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, GuessActivity.class));
+            }
+        });
+
+        btnSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SettingsActivity.class));
+            }
+        });
+
+        btnAbout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, AboutActivity.class));
+            }
+        });
+    }
+}
