405ab9450a116c5c43e42e408aa84e6d8fe3019e
LorenceNoel <167948703+LorenceNoel@users.noreply.github.com>
2025-03-27 23:05:26 -0600

    Updated layout and colorpalette for menu

--- app/src/main/java/com/example/assignment2_group2/MainActivity.java
+++ app/src/main/java/com/example/assignment2_group2/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,9 +13,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        TextView title = findViewById(R.id.txtTitle);
         Button btnStartGame = findViewById(R.id.btnStartGame);
         Button btnSettings = findViewById(R.id.btnSettings);
         Button btnAbout = findViewById(R.id.btnAbout);
+
+        title.setText("Guessing Game");
+        title.setTextColor(getResources().getColor(android.R.color.black));
+
+        btnStartGame.setBackgroundColor(getResources().getColor(android.R.color.holo_blue_light));
+        btnSettings.setBackgroundColor(getResources().getColor(android.R.color.darker_gray));
+        btnAbout.setBackgroundColor(getResources().getColor(android.R.color.black));
+        btnAbout.setTextColor(getResources().getColor(android.R.color.white));
 
         btnStartGame.setOnClickListener(new View.OnClickListener() {
             @Override
