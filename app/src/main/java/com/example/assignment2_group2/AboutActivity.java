ccdb764ac46d77e34ed45b7bf3c3cb6c4586e57e
Josshua Balita <129820359+josshuabalita@users.noreply.github.com>
2025-03-31 18:52:22 -0600

    About page completed. Please test all features and complete settings layout, functionalities.

--- app/src/main/java/com/example/assignment2_group2/AboutActivity.java
+++ app/src/main/java/com/example/assignment2_group2/AboutActivity.java
@@ -1,6 +1,9 @@
 package com.example.assignment2_group2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,15 @@ public class AboutActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button btnRtrnMain = findViewById(R.id.btnReturnMain);
+
+        btnRtrnMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(AboutActivity.this, MainActivity.class));
+            }
         });
     }
 }
